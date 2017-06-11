17e4cb228b59c6bc009f05a4fc374b83eadd5a42
Corecii Cyr <corecii@corecii.com>
2017-06-11 01:21:34 -0500

    Quick fix for updating.

--- README.md
+++ README.md
@@ -12,24 +12,23 @@ real `RobloxPlayerLauncher` through Steam.
 
 ## Building
 
-Building requires a Windows computer with an installation of [Rust](https://www.rust-lang.org/en-US/install.html), [Python 2.7](https://www.python.org/downloads/release/python-2713/),
-and [PyInstaller](http://www.pyinstaller.org/).  
-The two build scripts (`build.bat` and `build_release.bat`) will run `cargo build` and `pyinstaller -F -y` in the proper places and copy the executables
-to `build` or `build_release`.
+Building requires a Windows computer with an
+installation of [Rust](https://www.rust-lang.org/en-US/install.html),
+[Python 2.7](https://www.python.org/downloads/release/python-2713/), and [PyInstaller](http://www.pyinstaller.org/).  
+The two build scripts (`build.bat` and `build_release.bat`) will run `cargo build` and `pyinstaller -F -y`
+in the proper places and copy the executables to `build` or `build_release`.
 
 ## Issues
 
 * Python is used for calculating the game id of a game because I could not figure out how
 to set up the crc32 calculation properly in Rust, and I did not want to spend time
 writing my own. It was fastest to bundle a python executable.
-* When updating Roblox, it cannot be running underneath Steam or it will crash. `roblox_steam_launcher` will
-check if Roblox needs to be updated, and run `RobloxPlayerLauncher` with no arguments if
-it does. This is untested, but it *should* update Roblox, then open up the Games page.
-After updating, `roblox_steam_launcher` runs `RobloxPlayerLauncher` through Steam.
+* When updating, Roblox will run twice if Roblox Steam Launcher is installed.
+This is normal. One window is updating, and another is getting ready to play the game when it's updated.
 * The Roblox website will suggest that one downloads Roblox, as if it is not already installed.
 Roblox still runs fine. This may be an issue with the launcher taking too long, but it has
 not been investigated.
-* Errors are not handled well, and any executable included will likely error out and
+* Errors are not handled well, and any executable included will error out and
 exit without prompting the user if there is a problem.
 
 ## Screenshots
--- roblox_steam_launcher_master/src/main.rs
+++ roblox_steam_launcher_master/src/main.rs
@@ -1,7 +1,10 @@
 extern crate roblox_steam_launcher_shared;
+extern crate hyper;
 
 use roblox_steam_launcher_shared::*;
 use std::path::PathBuf;
+use hyper::client::Client;
+use std::io::Read;
 
 #[derive(Copy, Clone)]
 enum ApplyResult {
@@ -9,6 +12,14 @@ enum ApplyResult {
     AlreadyApplied,
     CannotApply,
     Error
+}
+
+fn check_if_newest_version(current_version: String) -> hyper::error::Result<bool> {
+    let client = Client::new();
+    let mut res = client.get("http://setup.roblox.com/version").send()?;
+    let mut msg = String::new();
+    res.read_to_string(&mut msg)?;
+    Ok(msg == current_version)
 }
 
 fn apply_launcher(version_path: &PathBuf) -> ApplyResult {
@@ -36,6 +47,25 @@ fn apply_launcher(version_path: &PathBuf) -> ApplyResult {
     return ApplyResult::Applied;
 }
 
+fn unapply_launcher(version_path: &PathBuf) -> ApplyResult {
+    let mut new_application_path = version_path.clone();
+    new_application_path.push("RobloxPlayerLauncher_original.exe");
+    if !new_application_path.is_file() {
+        return ApplyResult::AlreadyApplied;
+    }
+    let mut new_substitute_path = version_path.clone();
+    new_substitute_path.push("RobloxPlayerLauncher.exe");
+    if new_substitute_path.exists() {
+        if std::fs::remove_file(&new_substitute_path).is_err() {
+            return ApplyResult::Error;
+        }
+    }
+    if std::fs::rename(&new_application_path, new_substitute_path).is_err() {
+        return ApplyResult::Error;
+    }
+    return ApplyResult::Applied;
+}
+
 fn main() {
     let program_directory = match get_program_directory(&mut std::env::args()) {
         Some(path) => path,
@@ -46,6 +76,25 @@ fn main() {
     let mut config = match Config::from_path(&config_path) {
         Ok(config) => config,
         Err(err) => panic!("{}", err),
+    };
+    let current_version_directory = get_newest_roblox_player_directory_path(&program_directory).expect("Error getting newest directory.");
+    let current_version = current_version_directory.file_name().expect("Could not get file name!").to_string_lossy().into_owned();
+    match check_if_newest_version(current_version) {
+        Ok(false) => {
+            // Remove existing modifications
+            if let ApplyResult::Error = unapply_launcher(&current_version_directory) {
+                panic!("Error unapplying launcher.");
+            }
+            // Run roblox once to update, and wait for it to close.
+            // In the future, we should only run it once, but replace the new exe as it is created.
+            let mut exe_path = current_version_directory.clone();
+            exe_path.push("RobloxPlayerLauncher.exe");
+            let mut child_process = launch_game(&exe_path, &vec![]).expect("Could not launch Roblox");
+            if let Err(err) = child_process.wait() {
+                println!("Error waiting for Roblox: {:?}", err);
+            }
+        },
+        Ok(true) | Err(_) => (),  // if we are on the newest version OR if we couldn't check, launch roblox anyway.
     };
     let newest_version_directory = get_newest_roblox_player_directory_path(&program_directory).expect("Error getting newest directory.");
     match apply_launcher(&newest_version_directory) {
